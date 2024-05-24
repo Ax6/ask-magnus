7ce8d9aed3a81cdd208c5c308ddd9064ba1557c8
Ax_6 <axolo6@gmail.com>
2024-05-24 02:52:05 +0200

    arigato

--- analysis.py
+++ analysis.py
@@ -1,201 +1,89 @@
+import sys
+import time
 import chess
-import redis
-from src.engine import evaluate, ENGINE_OPTIONS
-from stockfish import Stockfish
-from datetime import datetime
+import asyncio
+import colorama
+import argparse
+from src.engine import evaluate, get_engine, get_db
 
-BIG_PIECES = [chess.QUEEN, chess.ROOK, chess.BISHOP, chess.KNIGHT]
 
-engine = Stockfish(**ENGINE_OPTIONS)
-db = redis.Redis()
+argparser = argparse.ArgumentParser(
+	"Analyze Lichess .pgn export",
+	"Specify path to game .pgn"
+)
+argparser.add_argument("pgn", help="Path to pgn file")
+argparser.add_argument(
+	"--limit", default=0, type=int,
+	help="Limit number of games to analyse")
 
-def extract_game_features(game):
-	data = []
-	"""
-	Features per move:
-	- game_id
-	- game_date
-	- my_color
-	- opponent_color
-	- my_rating
-	- opponent_rating
-	- my_rating_gain
-	- opponent_rating_gain
-	- variant
-	- time_control
-	- opening
-	- termination
-	- result
-	- my_time
-	- opponent_time
-	- half_move_number
-	- move_number
-	- move
-	- is_my_move
-	- is_best_move
-	- is_top_three_move
-	- is_top_five_move
-	- is_capture
-	- is_piece_sacrifice
-	- is_inaccuracy
-	- is_mistake
-	- is_blunder
-	- is_missed_mate
-	- piece_sacrificed
-	- absolute_centipawn_evaluation
-	- relative_centipawn_evaluation
-	- absolute_centipawn_loss
-	- relative_centipawn_loss
-	- mate_in
-	- board
-	"""
-	game_id = game.headers['Site'].split('/')[-1]
-	game_date = datetime.strptime(game.headers['UTCDate'] + ' ' + game.headers['UTCTime'], '%Y.%m.%d %H:%M:%S')
-	my_color = 'White' if game.headers['White'] == 'Ax_6' else 'Black'
-	opponent_color = 'Black' if my_color == 'White' else 'White'
-	my_rating = int(game.headers[my_color + 'Elo'])
-	opponent_rating = int(game.headers[opponent_color + 'Elo'])
-	my_rating_gain = int(game.headers.get(my_color + 'RatingDiff', 0))
-	opponent_rating_gain = int(game.headers.get(opponent_color + 'RatingDiff', 0))
-	variant = game.headers['Variant']
-	time_control = game.headers['TimeControl']
-	opening = game.headers['Opening']
-	termination = game.headers['Termination']
-	result = game.headers['Result'].split('-')[0 if my_color == 'White' else 1]
 
-	board = game.board()
-	previous_evaluation = evaluate(db, engine, board)
-	moves = list(game.mainline_moves())
-	color_to_move = 'White'
-	for i, (move, others_move, next_move) in enumerate(zip(moves, moves[1:] + [None], moves[2:] + [None, None])):
-		is_my_move = color_to_move == my_color
-		clock_before = game.clock()
-		piece_moved = board.piece_at(move.from_square)
-		piece_captured = board.piece_at(move.to_square)
-		if board.is_en_passant(move):
-			piece_captured = chess.Piece(chess.PAWN, chess.WHITE) if color_to_move == 'White' else chess.Piece(chess.PAWN, chess.BLACK)
-		is_capture = board.is_capture(move)
+def pretty_fen(fen):
+	pretty_pieces = {
+		'k': '♚', 'q': '♛', 'r': '♜', 'b': '♝', 'n': '♞', 'p': '♟︎',
+		'K': '♔', 'Q': '♕', 'R': '♖', 'B': '♗', 'N': '♘', 'P': '♙'
+	}
+	board = fen.split(' ')[0]
+	text = ''
+	global is_white
+	is_white = True
 
-		board.push(move)
+	def next_tile(contents):
+		global is_white
+		bg = colorama.Back.WHITE if is_white else colorama.Back.CYAN
+		is_white = not is_white
+		return bg + contents + ' ' + colorama.Back.RESET
 
-		clock_after = game.clock()
-		color_to_move = 'Black' if color_to_move == 'White' else 'White'
-		
-		evaluation = evaluate(db, engine, board)
-
-		my_time = clock_after if is_my_move else clock_before
-		opponent_time = clock_after if not is_my_move else clock_before
-		move_number = board.fullmove_number
-
-		top_moves = [top_moves['Move'] for top_moves in previous_evaluation['top_moves']]
-		if len(top_moves) > 0:
-			is_best_move = top_moves[0] == move.uci()
-			is_top_three_move = move.uci() in top_moves[:3]
-			is_top_five_move = move.uci() in top_moves[:5]
+	for letter in board:
+		if letter == '/':
+			text += '\n'
+			is_white = not is_white
+		elif letter.isdigit():
+			text += ''.join([next_tile(' ') for _ in range(int(letter))])
 		else:
-			raise Exception('Unexpected situation')
-		
-		moved_big_piece = piece_moved.piece_type in BIG_PIECES
-		captured_pawn = is_capture and piece_captured.piece_type == chess.PAWN
-		others_move_recapture = others_move and others_move.to_square == move.to_square
-		next_move_recapture = next_move and next_move.to_square == move.to_square
-		if moved_big_piece and captured_pawn and others_move_recapture and not next_move_recapture:
-			is_piece_sacrifice = True
-			piece_sacrificed = piece_moved.symbol()
-		else:
-			is_piece_sacrifice = False
-			piece_sacrificed = None
-		
-		previous_evaluation_type = previous_evaluation['eval']['type']
-		previous_evaluation_value = previous_evaluation['eval']['value']
-		current_evaluation_value = evaluation['eval']['value']
-		current_evaluation_type = evaluation['eval']['type']
+			text += next_tile(colorama.Fore.BLACK + pretty_pieces[letter] + colorama.Fore.RESET)
+	return colorama.Style.NORMAL  + text + colorama.Style.RESET_ALL
 
-		if current_evaluation_type != 'mate':
-			absolute_centipawn_evaluation = current_evaluation_value / 100
-			relative_centipawn_evaluation = -absolute_centipawn_evaluation if color_to_move == 'White' else absolute_centipawn_evaluation
-		else:
-			absolute_centipawn_evaluation = None
-			relative_centipawn_evaluation = None
 
-		if current_evaluation_type == 'mate':
-			is_missed_mate = False
-			mate_in = current_evaluation_value
-			absolute_centipawn_loss = None
-			relative_centipawn_loss = None
-			is_inaccuracy = False
-			is_mistake = False
-			is_blunder = False
-		elif previous_evaluation_type == 'mate' and current_evaluation_type != 'mate':
-			is_missed_mate = True
-			mate_in = None
-			absolute_centipawn_loss = None
-			relative_centipawn_loss = None
-			is_inaccuracy = relative_centipawn_evaluation > 50
-			is_mistake = relative_centipawn_evaluation > 10 and relative_centipawn_evaluation <= 50
-			is_blunder = relative_centipawn_evaluation <= 10
-		else:
-			is_missed_mate = False
-			mate_in = None
-			absolute_centipawn_loss = (current_evaluation_value - previous_evaluation_value) / 100
-			relative_centipawn_loss = -absolute_centipawn_loss if color_to_move == 'White' else absolute_centipawn_loss
-			is_inaccuracy = relative_centipawn_loss < -0.75 and relative_centipawn_loss >= -1
-			is_mistake = relative_centipawn_loss < -1 and relative_centipawn_loss >= -2
-			is_blunder = relative_centipawn_loss < -2
+async def main(pgn_path, limit_games=None):
+	pgn = open(pgn_path)
+	game_counter = 1
+	print_lines_to_clear = 0
+	engine = await get_engine()
+	db = await get_db()
 
-		if (current_evaluation_type == 'mate' and current_evaluation_value == 0) or board.is_stalemate():
-			win_probability = None
-			draw_probability = None
-			lose_probability = None
-		else:
-			_me_stat_index = 0 if my_color == 'White' else 2
-			_others_stat_index = 0 if _me_stat_index == 2 else 2
-			try:
-				win_probability = evaluation['wdl_stats'][_me_stat_index]
-			except:
-				breakpoint()
-			draw_probability = evaluation['wdl_stats'][1]
-			lose_probability = evaluation['wdl_stats'][_others_stat_index]
+	while game := chess.pgn.read_game(pgn):
+		board = game.board()
+		for move in game.mainline_moves():
+			if print_lines_to_clear > 0:
+				sys.stdout.write(f"\033[{print_lines_to_clear}A")
+			board.push(move)
+			start = time.time()
+			evaluation = await evaluate(db, engine, board)
+			end = time.time()
 
-		data.append({
-			'game_id': game_id,
-			'game_date': game_date,
-			'my_color': my_color,
-			'opponent_color': opponent_color,
-			'my_rating': my_rating,
-			'opponent_rating': opponent_rating,
-			'my_rating_gain': my_rating_gain,
-			'opponent_rating_gain': opponent_rating_gain,
-			'variant': variant,
-			'time_control': time_control,
-			'opening': opening,
-			'termination': termination,
-			'result': result,
-			'my_time': my_time,
-			'opponent_time': opponent_time,
-			'half_move_number': i + 1,
-			'move_number': move_number,
-			'move': move.uci(),
-			'is_my_move': is_my_move,
-			'is_best_move': is_best_move,
-			'is_top_three_move': is_top_three_move,
-			'is_top_five_move': is_top_five_move,
-			'is_capture': is_capture,
-			'is_piece_sacrifice': is_piece_sacrifice,
-			'is_inaccuracy': is_inaccuracy,
-			'is_mistake': is_mistake,
-			'is_blunder': is_blunder,
-			'is_missed_mate': is_missed_mate,
-			'piece_sacrificed': piece_sacrificed,
-			'absolute_centipawn_evaluation': absolute_centipawn_evaluation,
-			'relatice_centipawn_evaluation': relative_centipawn_evaluation,
-			'absolute_centipawn_loss': absolute_centipawn_loss,
-			'relative_centipawn_loss': relative_centipawn_loss,
-			'mate_in': mate_in,
-			'win_probability': win_probability,
-			'draw_probability': draw_probability,
-			'lose_probability': lose_probability,
-			'board': board.copy()
-		})
-		previous_evaluation = evaluation
-	return data
+			print(colorama.Fore.BLUE + f'Game {game_counter}' + colorama.Fore.RESET)
+			print(pretty_fen(board.fen()))
+			print(f" Time to evaluate: {end - start:.2f} seconds")
+			text = f" Evaluation: {evaluation[0]['score']}"
+			if not (board.is_stalemate() or board.is_checkmate()):
+				top_moves = []
+				for line in evaluation:
+					if board.is_stalemate() or board.is_checkmate():
+						break
+					if len(line['pv']) > 0:
+						top_moves.append(str(line['pv'][0]))
+				if len(top_moves) > 0:
+					text += f" | Top moves: {', '.join(top_moves)}"	
+			print(text)
+			print_lines_to_clear = 11
+
+		# Exit if we reach game count limit
+		game_counter += 1
+		if limit_games is not None and game_counter > limit_games:
+			break
+	await engine.quit()
+
+if __name__ == "__main__":
+	args = argparser.parse_args()
+	limit = args.limit if args.limit > 0 else None
+	asyncio.run(main(args.pgn, limit_games=limit))
--- src/engine.py
+++ src/engine.py
@@ -16,58 +16,20 @@ with await engine.analysis(chess.Board() ...) as analysis:
 import sys
 import time
 import chess
+import redis
 import pickle
 import asyncio
 import colorama
 import chess.pgn
 import chess.engine
 import redis.asyncio
-import redis.asyncio
 
 
-PGN_PATH = "data/lichess.pgn"
 ENGINE_CONFIG = {
 	"Threads": 6,
 	"Hash": 4096,
 }
 ENGINE_DEPTH = 5
-
-
-def pretty_fen(fen):
-	pretty_pieces = {
-		'k': '♚',
-		'q': '♛',
-		'r': '♜',
-		'b': '♝',
-		'n': '♞',
-		'p': '♟︎',
-		'K': '♔',
-		'Q': '♕',
-		'R': '♖',
-		'B': '♗',
-		'N': '♘',
-		'P': '♙'
-	}
-	board = fen.split(' ')[0]
-	text = ''
-	global is_white
-	is_white = True
-
-	def next_tile(contents):
-		global is_white
-		bg = colorama.Back.WHITE if is_white else colorama.Back.CYAN
-		is_white = not is_white
-		return bg + contents + ' ' + colorama.Back.RESET
-
-	for letter in board:
-		if letter == '/':
-			text += '\n'
-			is_white = not is_white
-		elif letter.isdigit():
-			text += ''.join([next_tile(' ') for _ in range(int(letter))])
-		else:
-			text += next_tile(colorama.Fore.BLACK + pretty_pieces[letter] + colorama.Fore.RESET)
-	return colorama.Style.NORMAL  + text + colorama.Style.RESET_ALL
 
 
 def clear_cache(db: redis.Redis):
@@ -77,15 +39,14 @@ def clear_cache(db: redis.Redis):
 
 
 async def evaluate(
-	engine: chess.engine.UciProtocol,
 	db: redis.asyncio.Redis,
+	engine: chess.engine.UciProtocol,
 	board: chess.Board,
 	multipv:int=3
 ):
 	key = f"ask-magnus:{board.fen()}:{engine.id}:{ENGINE_DEPTH}"
 	limit = chess.engine.Limit(depth=ENGINE_DEPTH)
-	is_cached = await db.exists(key); 
-	if is_cached:
+	if await db.exists(key):
 		raw_data = await db.get(key)
 		result = pickle.loads(raw_data)
 	else:
@@ -96,55 +57,17 @@ async def evaluate(
 		# result object it's very well developed and all __repr__ are
 		# reinstantiatable. So, I'll pickle. Fingers crossed.
 		await db.set(key, pickle.dumps(result))
-	return is_cached, result
+	return result
 
 
-async def main(limit_games=None):
-	pgn = open(PGN_PATH)
-	game_counter = 1
-	print_lines_to_clear = 0
-	
+async def get_engine():
 	# Get chess engine
 	transport, engine = await chess.engine.popen_uci("/opt/homebrew/bin/stockfish")
 	await engine.configure(ENGINE_CONFIG)
+	return engine
 
+
+async def get_db():
 	# Get database connection
 	db = await redis.asyncio.Redis()
-
-	while game := chess.pgn.read_game(pgn):
-		board = game.board()
-		for move in game.mainline_moves():
-			if print_lines_to_clear > 0:
-				sys.stdout.write(f"\033[{print_lines_to_clear}A")
-			board.push(move)
-			start = time.time()
-			is_cached, evaluation = await evaluate(engine, db, board)
-			end = time.time()
-
-			print(colorama.Fore.BLUE + f'Game {game_counter}' + colorama.Fore.RESET)
-			if is_cached:
-				print_lines_to_clear = 1
-				continue
-			print(pretty_fen(board.fen()))
-			print(f" Time to evaluate: {end - start:.2f} seconds")
-			text = f" Evaluation: {evaluation[0]['score']}"
-			if not (board.is_stalemate() or board.is_checkmate()):
-				top_moves = []
-				for line in evaluation:
-					if board.is_stalemate() or board.is_checkmate():
-						break
-					if len(line['pv']) > 0:
-						top_moves.append(str(line['pv'][0]))
-				if len(top_moves) > 0:
-					text += f" | Top moves: {', '.join(top_moves)}"	
-			print(text)
-			print_lines_to_clear = 11
-
-		# Exit if we reach game count limit
-		game_counter += 1
-		if limit_games is not None and game_counter > limit_games:
-			break
-	await engine.quit()
-
-if __name__ == "__main__":
-	asyncio.run(main())
+	return db
